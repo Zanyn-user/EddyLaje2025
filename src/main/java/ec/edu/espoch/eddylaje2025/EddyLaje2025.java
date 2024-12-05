aa7f241f11cb34087756d1c8770884104c5c3cb2
Zanyn-user <zanynuser@gmail.com>
2024-12-05 14:05:14 -0500

    creacion del proyecto

---
+++ src/main/java/ec/edu/espoch/eddylaje2025/EddyLaje2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.eddylaje2025;
+
+/**
+ *
+ * @author User
+ */
+public class EddyLaje2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
